e634297af65c8f3ac3224b28dc83b969a4d79e66
sabdulass <48061544+sabdulass@users.noreply.github.com>
2019-02-28 16:56:59 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("550677148705357826")
+setInterval(function() {
+channel.send(`اكس اكس اكس العمودي`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
